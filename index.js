0acccc2ace89b7b4a93871db9317a0bcb074d45d
Latifah Zawedde <zawedde2021@gmail.com>
2022-09-14 20:39:03 -0700

    4commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const employee = require('./lib/employee.js')
-const manager = require("./lib/manager");
-const engineer = require("./lib/engineer");
-const intern = require("./lib/intern");
-const generatehtml = require('./src/generat.js')
+//const employee = require("./lib/Employee.js");
+// const manager = require("./lib/Manager.js");
+// const engineer = require("./lib/Engineer.js");
+// const intern = require("./lib/Intern.js");
+const generatehtml = require('./src/generat');
 
 //Questions the user will be asked
 let questions = [
@@ -14,9 +14,9 @@ let questions = [
     //     name: 'role',
     // },
     {
-    type: 'input',
-    message: "What is your name",
-    name: 'name',
+        type: 'input',
+        message: "What is your name",
+        name: 'name',
     },
     {
         type: 'input',
@@ -33,18 +33,24 @@ let questions = [
         name: 'employeeType',
         message: 'What is the employee type?',
         choices: ['Manager', 'Engineer', 'Intern']
-    }
+    },
     {
         type: 'list',
         name: 'employeeType',
         message: 'What is the employee type?',
         choices: ['Employ','Manager', 'Engineer', 'Intern']
-    }
+    },
     {
         type: 'input',
         message: "What is your office number?",
         name: 'office',
-        validate: (value) => {if(value){return "manager"} else {return ""}}
+        //validate: (value) => {if(value){return "manager"} else {return ""}}
+        validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter manager's name." }
+        },
+
     },
     {
         type: 'input',
